4d0a24c505fc0b1a7f45e522093e9d8e195b9220
moudroff <moudroff>
2021-02-12 12:38:30 +0300

    SignalR

---
+++ ChatSample/ChatSample/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace ChatSample.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+    }
+}
