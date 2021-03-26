fba53e133147e2a67b4e31879570193c07fc1691
moudroff <moudroff>
2021-03-26 13:26:09 +0300

    Web Api Service

---
+++ IdentityApp/IdentityApp/Models/MyUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityApp.Models
+{
+    public class MyUser:IdentityUser
+    {
+        public int MyProperty { get; set; }
+
+    }
+}
---
+++ WebApiApp/WebApiApp/Models/TodoItem.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiApp.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+
+
+    public class TodoItem
+    {
+            public long Id { get; set; }
+            public string Name { get; set; }
+            public bool IsComplete { get; set; }
+    
+    }
+}
